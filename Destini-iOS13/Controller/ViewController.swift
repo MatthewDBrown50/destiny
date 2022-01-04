95fa98daa246ef0878b664bffd9de0a3abb510a6
MatthewDBrown50 <MatthewDBrown50@gmail.com>
2022-01-04 15:05:20 -0500

    initial completion

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
 
+    var storyBrain = StoryBrain()
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        updateUI(0)
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        let choice1 = storyBrain.getStories()[storyBrain.getStoryNumber()].getChoice1()
+        
+        if sender.titleLabel?.text == choice1 {
+            updateUI(1)
+        } else {
+            updateUI(2)
+        }
+    }
+    
+    func updateUI(_ choice: Int) {
+        
+        if choice == 1 || choice == 2{
+            storyBrain.nextStory(choice)
+        }
+        
+        let storyNumber = storyBrain.getStoryNumber()
+        let stories = storyBrain.getStories()
+        
+        textLabel.text = stories[storyNumber].getTitle()
+        topButton.setTitle(stories[storyNumber].getChoice1(), for: .normal)
+        bottomButton.setTitle(stories[storyNumber].getChoice2(), for: .normal)
+    }
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+    
+    init(title: String, choice1: String, choice1Destination: Int, choice2: String, choice2Destination: Int) {
+        self.title = title
+        self.choice1 = choice1
+        self.choice1Destination = choice1Destination
+        self.choice2 = choice2
+        self.choice2Destination = choice2Destination
+    }
+    
+    func getTitle() -> String {
+        return title
+    }
+    
+    func getChoice1() -> String {
+        return choice1
+    }
+    
+    func getChoice2() -> String {
+        return choice2
+    }
+    
+    func getChoice1Destination() -> Int{
+        return choice1Destination
+    }
+    
+    func getChoice2Destination() -> Int{
+        return choice2Destination
+    }
+}
